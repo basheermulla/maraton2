d879322be28403fa2b32838ee0adabb47e1baaf8
basheer mulla <Moranp98@gmail.com>
2023-08-01 19:52:23 +0300

    Getting Started with Create React App

--- remove_bg/src/Bg.js
+++ remove_bg/src/Bg.js
@@ -1,6 +1,18 @@
 import './Bg.css';
+import { useState } from "react";
 
 function Bg() {
+
+  const [display_no_bg_tab, setdisplay_no_bg_tab] = useState("no");
+
+  const change_tab = () => {
+    if (display_no_bg_tab == 'yes') {
+      setdisplay_no_bg_tab('no');
+    } else {
+      setdisplay_no_bg_tab('yes');
+    }
+
+  }
   return (
     <div className="Bg">
 
@@ -13,24 +25,45 @@ function Bg() {
       <div className='main_div'>
 
         <div className='left_div'>
-          LEFT
+          <div className='main_div_tabs_header'>
+            <span onClick={change_tab} className='no_bg'> הסר רקע </span>
+            <span onClick={change_tab} className='original'> מקורי </span>
+          </div>
+
+          {display_no_bg_tab == 'yes' ?
+            <div className='original_tab'>
+              original_tab
+            </div>
+            :
+            <div className='no_bg_tab'>
+              no_bg_tab
+            </div>
+          }
+
+
+
         </div>
 
         <div className='right_div'>
           <div className="right_div_middle_div">
             <div className="right_div_top">
               <div className="right_div_top_text"> תמונה חינם </div>
-              <div className="right_div_top_subtext">  612X408 תצוגה מקדימה של תמונה </div>
+              <div className="right_div_top_subtext">  612x408 תצוגה מקדימה של תמונה </div>
               <button className="right_div_top_btn"> הורד </button>
               <div className="right_div_top_sub_sub_text"> איכות טובה עד 0.25 מגה פיקסל </div>
             </div>
-            <div className="right_div_bottom"></div>
+            <div className="right_div_bottom">
+              <div className="right_div_bottom_text"> Pro </div>
+              <div className="right_div_bottom_subtext">  1280x1920 תמונה מלאה </div>
+              <button className="right_div_bottom_btn"> HD הורד </button>
+              <div className="right_div_bottom_sub_sub_text"> האיכות הטובה ביותר עד 25 מגה פיקסל </div>
+            </div>
           </div>
         </div>
-        
+
       </div>
 
-    </div>
+    </div >
   );
 }
 
